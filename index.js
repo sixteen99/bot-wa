cec6cddc4211d98b6b17d3274f301df67154de47
sixteen99 <rasixteen99@gmail.com>
2024-11-26 10:23:34 +0800

    test

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const { default: axios } = require('axios');
 
 const COOLDOWN_TIME = 1 * 1000; 
 const cooldownMap = new Map(); // Map untuk cooldown
+const me = '6285757895223@c.us';
 
 // Inisialisasi client WhatsApp
 const client = new Client({
@@ -18,6 +19,13 @@ const client = new Client({
 client.on('qr', (qr) => {
     qrcode.generate(qr, { small: true });
 });
+
+client.on("ready", async () => {
+    console.log("Bot telah terhubung!");
+
+    await client.sendMessage(me, 'Bot telah terhubung!');
+});
+
 
 function getWaktuSekarang() {
     const now = new Date();
@@ -36,63 +44,32 @@ function getWaktuSekarang() {
 
 // Menangani pesan yang diterima
 client.on('message', async (message) => {
-    if (!message.body || message.body.trim() === '') {
-        console.log('Pesan kosong, tidak diproses.');
-        return;
-    }
+    try {
+        const chatId = message.from; // Mendapatkan ID chat pengirim
 
-    console.log('Pesan diterima: ', message.body);
-
-    const senderNumber = message.from.replace('@c.us', '');
-    const currentTime = Date.now();
-
-    // Cooldown
-    if (cooldownMap.has(senderNumber)) {
-        const lastMessageTime = cooldownMap.get(senderNumber);
-
-        if (currentTime - lastMessageTime < COOLDOWN_TIME) {
-            console.log(`Pesan dari ${senderNumber} diabaikan karena masih dalam cooldown.`);
-            return;
-        }
-    }
-
-    cooldownMap.set(senderNumber, currentTime);
-
-    const waktuSekarang = getWaktuSekarang();
-    const contact = await message.getContact();
-    console.log("nama: " + contact.pushname);
-
-    // Membalas dengan teks tertentu berdasarkan pesan
-    if (message.body.startsWith('#')) {
-        const searchQuery = message.body.slice(1).trim();
-        axios.get(`https://api.ryzendesu.vip/api/search/pinterest?query=${searchQuery}`).then( async (res) => {
-            // console.log(res.data);
-            const results = res.data
-            if (results && results.length > 0) {
-                // Looping dan kirim setiap hasil
-                for (let index = 0; index < results.length; index++) {
-                    const media = await MessageMedia.fromUrl(results[index]); // Gunakan dari URL
-                    console.log(media);
-                    await message.reply(media);
-                }
-            } else {
-                // Jika tidak ada hasil
-                message.reply('Tidak ditemukan hasil untuk pencarian tersebut.');
+        // Memvalidasi apakah pesan berasal dari grup, chat pribadi, atau status
+        if (chatId.includes('@g.us')) {
+            // console.log(mentions);
+            const mentions = message.mentionedIds; // Mendapatkan ID yang ditandai dalam pesan
+            // Memeriksa apakah ID Anda ada dalam daftar mentionedIds
+            if (mentions.includes('6285757895223@c.us')) {
+                console.log("Anda ditandai dalam pesan grup!");
+                message.reply("Terima kasih sudah menandai saya!");
+                await client.sendMessage(me, 'Bot telah terhubung!');
             }
-        }).catch((err) => {
-            // message.reply(err.error);
-            console.log(err.error);
-        });
-    // } else if (message.body.toLowerCase() === 'apa kabar') {
-    //     message.reply('Saya baik, terima kasih! Bagaimana dengan kamu?');
-    // } else if (message.body.toLowerCase().includes('gambar')) {
-    //     const media = MessageMedia.fromFilePath('img/fuji.jpeg'); // Path ke gambar
-    //     message.reply(media).catch(err => console.error('Error mengirim gambar:', err));
-    } else {
-        message.reply(`Selamat ${waktuSekarang}.. \nPesan anda akan dibalas saat pemilik bangun/online.\n\n_*Dibalas oleh: Bot Haru._`);
-    }
 
+        } else if (chatId.includes('@c.us')) {
+            console.log("Pesan berasal dari chat pribadi");
 
+        } else if (message.isStatus) {
+            console.log("Pesan berasal dari status");
+            message.reply("Pesan diterima dari status.");
+        } else {
+            console.log("Sumber pesan tidak diketahui");
+        }
+    } catch (error) {
+        await client.sendMessage('6285757895223@c.us', error);
+    }  
 });
 
 // Inisialisasi dan mulai client
